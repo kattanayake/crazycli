9050a16f5df4d72c98dcf5230cc53df9a9c127d3
kushantha <kushantha@utexas.edu>
2018-04-19 01:00:15 -0500

    init

---
+++ getfile.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/ttm436/socialdrone/master/cfcli/cfcli.py?token=AU8exqUG7npJXPr7-VxWWRfeYRkpMglRks5a4WeiwA%3D%3D
+mv cfcli.py?token=AU8exqUG7npJXPr7-VxWWRfeYRkpMglRks5a4WeiwA%3D%3D cfcli.py
